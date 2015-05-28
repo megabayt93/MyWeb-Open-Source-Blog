e583dde36f63c970f5b702f1bf3c2401605aa891
https://github.com/megabayt93/ShowSystemInfo.git <fatihyazici1010@gmail.com>
2015-05-29 02:19:32 +0300

    updateContactMail
    updateContactMail

--- MWOSB/MyWeb/Models/ModelContact.cs
+++ MWOSB/MyWeb/Models/ModelContact.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using MyWebEntityLibrary;
 using MyWebEntityLibrary.MessageEntity;
@@ -20,13 +21,15 @@ namespace MyWeb.Models
 
         public object ComingContact()
         {
-            var contactInformation = (from p in _contactContext.Contacts select p).FirstOrDefault(cId => cId.ContactId > 0);
+            var contactInformation =
+                (from p in _contactContext.Contacts select p).FirstOrDefault(cId => cId.ContactId > 0);
             return contactInformation;
         }
 
         public string Send(string nameSurname, string mail, string content)
         {
             string info = "";
+
             try
             {
                 _messagesTable.NameSurname = nameSurname;
@@ -36,14 +39,42 @@ namespace MyWeb.Models
                 _contactContext.Messages.Add(_messagesTable);
                 _contactContext.SaveChanges();
                 info = "$('#sendDiv').html('Mesaj Gönderildi!');";
-                return info;
+
+
+                var mailControl = _contactContext.Mails.OrderBy(m => m.MailId > 0).FirstOrDefault();
+                if (mailControl!=null)
+                {
+                    SmtpClient smtpServer = new SmtpClient(mailControl.MailSmtp, mailControl.MailPort);
+                    var sendMail = new MailMessage();
+                    sendMail.From = new MailAddress(mailControl.MailAdress);
+                    sendMail.To.Add(mailControl.MailAdress);
+                    sendMail.Subject = "Mesaj Gönderildi";
+                    sendMail.IsBodyHtml = true;
+                    sendMail.Body =
+                        "<html><body><b>YENİ BİR MESAJ VAR!</b><br><br><b>Bilgiler Aşağıdaki Gibidir.</b><br><br>Mesaj Gönderenin<br> İsmi: " +
+                        nameSurname + " <br>e-Mail: " + mail + "<br>Mesaj: " + content + "</body></html>";
+                    smtpServer.EnableSsl = true;
+                    smtpServer.UseDefaultCredentials = false;
+                    smtpServer.Credentials = new System.Net.NetworkCredential(mailControl.MailAdress,
+                        mailControl.MailPassword);
+                    smtpServer.Send(sendMail);
+                    return info;
+                }
+                else
+                {
+                   
+                    return info;
+                }
+             
             }
-            catch (Exception)
+            catch (Exception ex)
             {
                 info = "$('#sendDiv').html('HATA! Mesaj Gönderilemedi!');";
-                return info; throw;
+                return info;
             }
 
+
         }
+
     }
 }
